8d369d4ad6f465958d47671f31a3ad8ce15f90c5
Jonas Rafn <jonaschrafn@gmail.com>
2015-11-03 14:24:08 +0100

    Merge origin/master
    Conflicts:
    	AngSeedClient/app/index.html
    	AngSeedServer/nbproject/build-impl.xml
    	AngSeedServer/nbproject/genfiles.properties
    	AngSeedServer/nbproject/project.properties
    	AngSeedServer/nbproject/project.xml

--- AngSeedClient/app/views/create_user/create_user.js
+++ AngSeedClient/app/views/create_user/create_user.js
@@ -2,13 +2,18 @@
 
 angular.module('myApp.CreateUser', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/create_user', {
-    templateUrl: 'views/create_user/create_user.html',
-    controller: 'CreateUserCtrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/create_user', {
+                    templateUrl: 'views/create_user/create_user.html',
+                    controller: 'CreateUserCtrl'
+                });
+            }])
 
-.controller('CreateUserCtrl', function() {
- 
-});
+        .controller('CreateUserCtrl', function ($http) {
+            var self = this;
+            self.user = {};
+            self.createUser = function () {
+                alert("hej");
+                $http.post("api/create", self.user);
+            };
+        });
