81ccdf9c495a0f98b4bdf469624e98b78cd55032
Tobias Jacobsen <tobias.cbs@gmail.com>
2015-11-03 13:54:09 +0100

    some more

---
+++ AngSeedServer/src/java/entity/User.java
@@ -0,0 +1,68 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "user")
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "User.findByUserName", query = "SELECT u FROM User u WHERE u.userName = :userName")})
+public class User implements Serializable {
+
+    @Id
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "roles")
+    @ElementCollection
+    List<String> roles = new ArrayList();
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User(String userName, String password, List<String> roles) {
+        this.userName = userName;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public void AddRole(String role) {
+        roles.add(role);
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
